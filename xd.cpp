e77b8e2e5e7c7be909785c0acaad86a93aacf780
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2024-11-24 01:23:00 -0500

    xd
    xd

--- xd.cpp
+++ xd.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
+
+
+static int id_sec = 1;
+static int id_pis = 1;
+static int id_sup = 1;
+static int id_dis = 1;
 
 struct disco;
 struct superficie;
@@ -12,16 +19,72 @@ static vector<pista*> todas_pistas;
 static vector<superficie*> todas_superficies;
 static vector<disco*> todos_discos;
 
+struct tipo {
+    int tam;
+    int objeto_id;
+    tipo(int t, int id){
+        tam=t;
+        objeto_id=id;
+    }
+    virtual void getvalor() = 0;
+};
+
+struct INTEGER:public tipo {
+    int val;
+    INTEGER(int val, int ID,int t):tipo(t,ID) {
+        this->val = val;
+    }
+    void getvalor() override{
+        cout<<"el valor es:"<<val<<endl;
+    }
+};
+struct VARCHAR: public tipo {
+    string val;
+    int longitud_maxima;
+    
+    VARCHAR(string val, int ID, int max):tipo(val.length(), ID) {
+        this->longitud_maxima = max;
+        if(val.length() <= max) {
+            this->val = val;
+        } else {
+            cout<<"no se pudo ingresar"<<endl;
+        }
+    }
+    
+    void getvalor() override {
+        cout << "el valor es:"<<val<<endl;
+    }
+};
+
+struct DECIMAL: public tipo {
+    double val;
+    int precision;
+    int escala;
+    
+    DECIMAL(double val, int ID, int precision, int escala):tipo(8, ID) {
+        this->val = val;
+        this->precision = precision;
+        this->escala = escala;
+    }
+    
+    void getvalor() override {
+        cout <<"el valor es:"<<val<< endl;
+    }
+};
+
+
+
 struct sector {
     int tamano;
     sector* nextsector;
     vector<int> objeto_ids;
-    
+    int ID_sector;
+
     sector(int tam) {
         tamano = tam;
         nextsector = nullptr;
+        ID_sector = id_sec++;
         todos_sectores.push_back(this);
-        
     }
 };
 
@@ -29,12 +92,14 @@ struct pista {
     vector<sector> sectores;
     pista* nextpista;
     vector<int> objeto_ids;
-    
+    int ID_pista;
+
     pista(int numSectores, int tamSector) {
         nextpista = nullptr;
+        ID_pista = id_pis++;
         todas_pistas.push_back(this);
-        
-        for(int i = 0; i < numSectores; i++) {
+
+        for (int i = 0; i < numSectores; i++) {
             sector xd(tamSector);
             sectores.push_back(xd);
         }
@@ -45,12 +110,14 @@ struct superficie {
     vector<pista> pistas;
     superficie* nextsuperficie;
     vector<int> objeto_ids;
-    
+    int ID_superficie;
+
     superficie(int numPistas, int numSectores, int tamSector) {
         nextsuperficie = nullptr;
+        ID_superficie = id_sup++;
         todas_superficies.push_back(this);
-        
-        for(int i = 0; i < numPistas; i++) {
+
+        for (int i = 0; i < numPistas; i++) {
             pista xd(numSectores, tamSector);
             pistas.push_back(xd);
         }
@@ -61,12 +128,14 @@ struct disco {
     vector<superficie> superficies;
     disco* nextdisco;
     vector<int> objeto_ids;
-    
+    int ID_disco;
+
     disco(int numPistas, int numSectores, int tamSector) {
         nextdisco = nullptr;
+        ID_disco = id_dis++;
         todos_discos.push_back(this);
-        
-        for(int i = 0; i < 2; i++) {
+
+        for (int i = 0; i < 2; i++) {
             superficie xd(numPistas, numSectores, tamSector);
             superficies.push_back(xd);
         }
@@ -82,12 +151,11 @@ struct dicodu {
     int numSectores;
     int tamSector;
 
-    
     sector* ultimo_sector_usado;
     pista* ultima_pista_usada;
     superficie* ultima_superficie_usada;
     disco* ultimo_disco_usado;
-    
+
     void enlazar_elementos() {
         ultimo_sector_usado = todos_sectores[0];
         ultima_pista_usada = todas_pistas[0];
@@ -109,7 +177,7 @@ struct dicodu {
             todas_pistas.back()->nextpista = nullptr;
         }
 
- 
+
         for(size_t i = 0; i < todas_superficies.size() - 1; i++) {
             todas_superficies[i]->nextsuperficie = todas_superficies[i + 1];
         }
@@ -117,7 +185,7 @@ struct dicodu {
             todas_superficies.back()->nextsuperficie = nullptr;
         }
 
- 
+
         for(size_t i = 0; i < todos_discos.size() - 1; i++) {
             todos_discos[i]->nextdisco = todos_discos[i + 1];
         }
@@ -125,51 +193,46 @@ struct dicodu {
             todos_discos.back()->nextdisco = nullptr;
         }
 
-  
+
         todos_sectores.clear();
         todas_pistas.clear();
         todas_superficies.clear();
         todos_discos.clear();
     }
-    
-    dicodu(int nDiscos, int nPistas, int nSectores, int tSector) {
-       
-        
 
-   
+    dicodu(int nDiscos, int nPistas, int nSectores, int tSector) {
         todos_sectores.clear();
         todas_pistas.clear();
         todas_superficies.clear();
         todos_discos.clear();
 
         numDiscos = nDiscos;
+        numSuperficies = 2;
         numPistas = nPistas;
         numSectores = nSectores;
         tamSector = tSector;
-        
+
         cout << "Creando dicodu con " << numDiscos << " discos" << endl;
-        for(int i = 0; i < numDiscos; i++) {
+        for (int i = 0; i < numDiscos; i++) {
             disco xd(numPistas, numSectores, tamSector);
             discos.push_back(xd);
         }
-        
-      
+
         enlazar_elementos();
-    }
-
-    
-
-    // Este método lo puedes usar después para actualizar los punteros
-    void actualizar_ultimo_usado(sector* s, pista* p, superficie* sup, disco* d) {
-        if(s) ultimo_sector_usado = s;
-        if(p) ultima_pista_usada = p;
-        if(sup) ultima_superficie_usada = sup;
-        if(d) ultimo_disco_usado = d;
     }
 };
 
 int main() {
     dicodu d(2, 3, 4, 10);
-     
+
+    
+    cout << "IDs asignados:\n";
+    cout << "Último sector: " << id_sec - 1 << endl;
+    cout << "Última pista: " << id_pis - 1 << endl;
+    cout << "Última superficie: " << id_sup - 1 << endl;
+    cout << "Último disco: " << id_dis - 1 << endl;
+
     return 0;
 }
+
+
